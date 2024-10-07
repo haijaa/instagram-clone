b145a34da172e7118c13bd5316a8b1355de55715
haijaa <antkarl11@gmail.com>
2024-10-07 09:02:10 +0200

    Mid morning commit

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,3 @@
-// app.js
 import express from "express";
 import cors from "cors";
 import userRoutes from "./routes/userRoutes.js";
--- components/Header.vue
+++ components/Header.vue
@@ -1,28 +1,36 @@
 <template>
-  <v-container class="d-flex flex-column justify-space-between" min-height="150px" max-width="80%" >
-    
+  <v-container
+    class="d-flex flex-column justify-space-between"
+    min-height="150px"
+    max-width="80%"
+  >
     <div class="storiescontainer overflow-auto">
       <p class="hidden-sm-and-down">For you | Feed</p>
-    <v-menu v-show="isSmAndDown">
-      <template v-slot:activator="{ props: activatorprops }">
-        <p v-bind="activatorprops" class="hover hidden-md-and-up">
-          For you <v-icon icon="mdi-arrow-down" size="15" />
-        </p>
-      </template>
-      <template v-slot:default="{ isActive }">
-        <v-list-item>Feed</v-list-item>
-      </template>
-    </v-menu>
-    <v-divider />
-   <v-container class="d-flex">
-      <v-container v-for="content in post.users" class="storiesSmallContainer">
-        <img
-          :src="content.profilePic"
-          style="width: 50px; height: 45px; border-radius: 50%"
-        />
-        <p class="thinSmall" style="max-width: 25px">{{ content.user }}</p>  
+      <v-menu v-show="isSmAndDown">
+        <template v-slot:activator="{ props: activatorprops }">
+          <p v-bind="activatorprops" class="hover hidden-md-and-up">
+            For you <v-icon icon="mdi-arrow-down" size="15" />
+          </p>
+        </template>
+        <template v-slot:default="{ isActive }">
+          <v-list-item>Feed</v-list-item>
+        </template>
+      </v-menu>
+      <v-divider />
+      <v-container class="d-flex">
+        <v-container
+          v-for="content in post.users"
+          class="storiesSmallContainer"
+        >
+          <img
+            :src="content.profilePic"
+            style="width: 50px; height: 45px; border-radius: 50%"
+          />
+          <p class="thinSmall" style="max-width: 25px">
+            {{ shortenName(content.user) }}
+          </p>
+        </v-container>
       </v-container>
-    </v-container>
     </div>
   </v-container>
 </template>
@@ -35,6 +43,9 @@ const display = useDisplay();
 
 const isMdAndUp = computed(() => display.mdAndUp);
 const isSmAndDown = computed(() => display.smAndDown);
+const shortenName = (name) => {
+  return name.length > 6 ? name.substring(0, 6) + ".." : name;
+};
 </script>
 
 <style>
@@ -51,5 +62,7 @@ const isSmAndDown = computed(() => display.smAndDown);
   margin-right: 5px;
   display: flex;
   flex-direction: column;
+  justify-content: center;
+  align-items: start;
 }
 </style>
