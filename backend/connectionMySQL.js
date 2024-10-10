c89588199ec75d709d67269921de51aa05d719f8
haijaa <antkarl11@gmail.com>
2024-10-10 11:08:51 +0200

    Fått åtkomst till min egen backend.

--- backend/connectionMySQL.js
+++ backend/connectionMySQL.js
@@ -1,9 +1,11 @@
-const mysql = require("mysql");
-const connectionMySQL = mysql.createConnection({
+import mysql from "mysql2/promise"; // Använd 'mysql2/promise' för att få en löfteskompatibel version
+
+const connectionMySQL = await mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "password",
   database: "instagram_clone",
+  port: 3306,
 });
 
-module.exports = connectionMySQL;
+export default connectionMySQL;
